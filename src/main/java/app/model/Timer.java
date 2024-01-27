e0282e2ba092b807bd026d8cfd7b0b41764aa2c9
seeewo4kin <sewo4kin321@gmail.com>
2024-01-27 19:46:39 +0300

    2.1.3

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,9 @@
 package app.model;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
@@ -7,4 +11,5 @@ public class Timer {
     public Long getTime() {
         return nanoTime;
     }
+
 }
